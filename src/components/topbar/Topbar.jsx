a147c2e957c82ccddb615e46b769ddfbbf902c9a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-03 17:27:47 +0900

    Material UI

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,3 +1,4 @@
+import { Chat, Notifications, Search } from "@mui/icons-material";
 import React from "react";
 
 export default function Topbar() {
@@ -8,6 +9,7 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
+          <Search className="searchIcon" />
           <input
             type="text"
             className="searchInput"
@@ -16,8 +18,14 @@ export default function Topbar() {
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
+        <div className="topbarIconItem">
+          <Chat />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbarIconItem">
+          <Notifications />
+          <span className="topbarIconBadge">1</span>
+        </div>
       </div>
       <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
     </div>
