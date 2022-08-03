3320bcc1d5dc582493974b9d304ad385f00f7520
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-03 16:39:33 +0900

    Homeコンポーネント作成

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">SNS App</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="お探しの物は何？"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarIconItem">1</div>
+        <div className="topbarIconItem">2</div>
+      </div>
+      <img src="/assets/person/1.jpg" alt="" className="topbarImg" />
+    </div>
+  );
+}
