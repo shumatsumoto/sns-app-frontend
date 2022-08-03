4075ab791c83ec243be598292cac9a7640f84fb8
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-03 16:50:33 +0900

    img

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -19,7 +19,7 @@ export default function Topbar() {
         <div className="topbarIconItem">1</div>
         <div className="topbarIconItem">2</div>
       </div>
-      <img src="/assets/person/1.jpg" alt="" className="topbarImg" />
+      <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
     </div>
   );
 }
