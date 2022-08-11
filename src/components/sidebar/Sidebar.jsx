d2cdd760b3bab315431b145b655e574a7563053d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-11 22:45:39 +0900

    Sidebar style

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,63 @@
+import {
+  Bookmark,
+  Home,
+  MessageRounded,
+  Notifications,
+  Person,
+  Search,
+  Settings,
+} from "@mui/icons-material";
 import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Home className="sidebarIcon" />
+            <span className="sidebarListItemText">ホーム</span>
+          </li>
+          <li className="sidebarListItem">
+            <Search className="sidebarIcon" />
+            <span className="sidebarListItemText">検索</span>
+          </li>
+          <li className="sidebarListItem">
+            <Notifications className="sidebarIcon" />
+            <span className="sidebarListItemText">通知</span>
+          </li>
+          <li className="sidebarListItem">
+            <MessageRounded className="sidebarIcon" />
+            <span className="sidebarListItemText">メッセージ</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">ブックマーク</span>
+          </li>
+          <li className="sidebarListItem">
+            <Person className="sidebarIcon" />
+            <span className="sidebarListItemText">プロフィール</span>
+          </li>
+          <li className="sidebarListItem">
+            <Settings className="sidebarIcon" />
+            <span className="sidebarListItemText">設定</span>
+          </li>
+        </ul>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt=""
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">Shu Matsumoto</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
