0104ecf651a4463abafc90bff42b4b4c9111f31d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-11 23:15:06 +0900

    Shareコンポーネント作成

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,8 +1,44 @@
+import { Analytics, Face, Gif, Image } from "@mui/icons-material";
 import React from "react";
 import "./Share.css";
 
 function Share() {
-  return <div>Share</div>;
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img src="/assets/person/1.jpeg" alt="" className="shareProfileImg" />
+          <input
+            type="text"
+            className="shareInput"
+            placeholder="いま何してるの？"
+          />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareButtons">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <Image className="shareIcon" />
+              <span className="shareOptionText">写真</span>
+            </div>
+            <div className="shareOption">
+              <Gif className="shareIcon" />
+              <span className="shareOptionText">GIF</span>
+            </div>
+            <div className="shareOption">
+              <Face className="shareIcon" />
+              <span className="shareOptionText">気持ち</span>
+            </div>
+            <div className="shareOption">
+              <Analytics className="shareIcon" />
+              <span className="shareOptionText">投票</span>
+            </div>
+            <button className="shareButton">投稿</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Share;
