27e5d12fa06c2d60149757b03276a8f0af20ced4
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-11 22:15:04 +0900

    Topbar style

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import { Chat, Notifications, Search } from "@mui/icons-material";
 import React from "react";
-import "./Topbar.css"
+import "./Topbar.css";
 
 export default function Topbar() {
   return (
@@ -19,16 +19,18 @@ export default function Topbar() {
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">
-          <Chat />
-          <span className="topbarIconBadge">1</span>
-        </div>
-        <div className="topbarIconItem">
-          <Notifications />
-          <span className="topbarIconBadge">1</span>
+        <div className="topbarItemIcons">
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
         </div>
       </div>
-      <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
     </div>
   );
 }
