295ddf52e82a2f4143599c3d9ed6ebe5f993da35
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-12 17:51:51 +0900

    Rightbar jsx

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import "./Rightbar.css";
 
 function RightBar() {
-  return <div className="rightbar">RightBar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="./assets/star.png" alt="" className="starImg" />
+          <span className="eventText">
+            <b>フォロワー限定</b>
+            イベント開催中！
+          </span>
+        </div>
+        <img src="./assets/event.jpeg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="./assets/person/1.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Shu Matsumoto</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default RightBar;
