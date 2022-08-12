32e9fcceda6908c7e7a4a702bc73dc6b8d97dd3e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-12 17:38:41 +0900

    Post style

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,17 +1,24 @@
 import { MoreVert } from "@mui/icons-material";
 import React from "react";
+import "./Post.css";
 
 function Post() {
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
-          <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
-          <span className="postUsername">Shu Matsumoto</span>
-          <span className="postDate">5分前</span>
-        </div>
-        <div className="postRight">
-          <MoreVert />
+          <div className="postTopLeft">
+            <img
+              src="./assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUsername">Shu Matsumoto</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
         <div className="postCenter">
           <span className="postText">SNSを自作中です。</span>
