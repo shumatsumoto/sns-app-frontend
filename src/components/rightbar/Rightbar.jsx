6dc30652b9ea650dcfb4bf768a0c1ab394e8a569
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-12 17:59:25 +0900

    Rightbar style

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -12,15 +12,30 @@ function RightBar() {
             イベント開催中！
           </span>
         </div>
-        <img src="./assets/event.jpeg" alt="" />
+        <img src="./assets/event.jpeg" alt="" className="eventImg" />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="./assets/person/1.jpeg" alt="" />
+              <img
+                src="./assets/person/1.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
               <span className="rightbarOnline"></span>
             </div>
             <span className="rightbarUsername">Shu Matsumoto</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/2.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tanaka</span>
           </li>
         </ul>
       </div>
