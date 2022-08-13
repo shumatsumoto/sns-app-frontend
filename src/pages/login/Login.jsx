6a578b7f3af1daa01c852fe7446f76227889c232
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-13 21:38:20 +0900

    Login　コンポーネント作成

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Real SNS</h3>
+          <span className="loginDesc">本格的なSNSを、自分の手で。</span>
+        </div>
+        <div className="loginRight">ああああ</div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
