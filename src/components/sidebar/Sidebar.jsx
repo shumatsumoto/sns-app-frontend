79c95859b8629b4b1451bf9aff60775a10e9982d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-13 19:05:18 +0900

    sidebar closefriend コンポーネント作成

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,9 @@ import {
   Settings,
 } from "@mui/icons-material";
 import React from "react";
+import CloseFriend from "../closefriend/CloseFriend";
 import "./Sidebar.css";
+import { Users } from "../../dummyData";
 
 function Sidebar() {
   return (
@@ -46,30 +48,9 @@ function Sidebar() {
         </ul>
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
-          <li className="sidebarFriend">
-            <img
-              src="/assets/person/2.jpeg"
-              alt=""
-              className="sidebarFriendImg"
-            />
-            <span className="sidebarFriendName">AAA</span>
-          </li>
-          <li className="sidebarFriend">
-            <img
-              src="/assets/person/3.jpeg"
-              alt=""
-              className="sidebarFriendImg"
-            />
-            <span className="sidebarFriendName">BBB</span>
-          </li>
-          <li className="sidebarFriend">
-            <img
-              src="/assets/person/4.jpeg"
-              alt=""
-              className="sidebarFriendImg"
-            />
-            <span className="sidebarFriendName">CCC</span>
-          </li>
+          {Users.map((user) => (
+            <CloseFriend user={user} key={user.id} />
+          ))}
         </ul>
       </div>
     </div>
