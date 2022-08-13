a3f29829e0436b95540f2f2a337a820ff21ff627
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-13 20:10:43 +0900

    Profile jsx

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -11,8 +11,30 @@ function Profile() {
       <Topbar />
       <div className="profile">
         <Sidebar />
-        <TimeLine />
-        <Rightbar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                src="./assets/post/3.jpeg"
+                alt=""
+                className="profileCoverImg"
+              />
+              <img
+                src="./assets/person/7.jpeg"
+                alt=""
+                className="profileUserImg"
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Shu Matsumoto</h4>
+              <span className="profileInfoDesc">Web Developer</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <TimeLine />
+            <Rightbar />
+          </div>
+        </div>
       </div>
     </>
   );
