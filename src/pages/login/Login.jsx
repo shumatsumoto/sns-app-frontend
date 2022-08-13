df01bf069635fdd287b084e7e453afd0cbc0ca6b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-13 21:45:41 +0900

    loginForm　jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,7 +9,20 @@ function Login() {
           <h3 className="loginLogo">Real SNS</h3>
           <span className="loginDesc">本格的なSNSを、自分の手で。</span>
         </div>
-        <div className="loginRight">ああああ</div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <p className="loginMessage">ログインはこちら</p>
+            <input type="text" className="loginInput" placeholder="Eメール" />
+            <input
+              type="text"
+              className="loginInput"
+              placeholder="パスワード"
+            />
+            <button className="loginButton">ログイン</button>
+            <span className="loginForgot">パスワードを忘れた方へ</span>
+            <button className="loginRegisterButton">アカウントを作成</button>
+          </div>
+        </div>
       </div>
     </div>
   );
