a24396ed21e8d9eaf6a162c60f747695d03b448e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-13 18:38:07 +0900

    dummydataを表示 posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,7 +2,8 @@ import { MoreVert } from "@mui/icons-material";
 import React from "react";
 import "./Post.css";
 
-function Post() {
+function Post({ post }) {
+  console.log(post);
   return (
     <div className="post">
       <div className="postWrapper">
@@ -14,23 +15,25 @@ function Post() {
               className="postProfileImg"
             />
             <span className="postUsername">Shu Matsumoto</span>
-            <span className="postDate">5分前</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">SNSを自作中です。</span>
-          <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+          <span className="postText">{post.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img src="./assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">5人がいいねを押しました</span>
+            <span className="postLikeCounter">
+              {post.like}がいいねを押しました
+            </span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">4:コメント</span>
+            <span className="postCommentText">{post.comment}:コメント</span>
           </div>
         </div>
       </div>
--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./TimeLine.css";
+import { Posts } from "../../dummyData";
 
 function TimeLine() {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        <Post />
+        {Posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
       </div>
     </div>
   );
