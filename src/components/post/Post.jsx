2343f35887e1c40cf5e6ae47162ad3e6048fc601
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-15 09:58:37 +0900

    post data fetching

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,23 @@
 import { MoreVert } from "@mui/icons-material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Post.css";
-import { Users } from "../../dummyData";
+import axios from "axios";
+// import { Users } from "../../dummyData";
 
 function Post({ post }) {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`/users/${post.userId}`);
+      // console.log(response);
+      setUser(response.data);
+    };
+    fetchUser();
+  }, []);
+
   const handleLike = () => {
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
@@ -17,18 +28,8 @@ function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src={
-                PUBLIC_FOLDER +
-                Users.filter((user) => user.id === post.userId)[0]
-                  .profilePicture
-              }
-              alt=""
-              className="postProfileImg"
-            />
-            <span className="postUsername">
-              {Users.filter((user) => user.id === post.userId)[0].username}
-            </span>
+            <img src={user.profilePicture} alt="" className="postProfileImg" />
+            <span className="postUsername">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
@@ -47,7 +48,9 @@ function Post({ post }) {
               className="likeIcon"
               onClick={() => handleLike()}
             />
-            <span className="postLikeCounter">{like}がいいねを押しました</span>
+            <span className="postLikeCounter">
+              {like}人がいいねを押しました
+            </span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment}:コメント</span>
