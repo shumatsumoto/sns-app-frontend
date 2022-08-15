4f0c2a21efca80a6b4979d3966165f2b22465de0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-15 22:47:03 +0900

    新規登録機能

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useRef } from "react";
 import "./Register.css";
 
@@ -7,8 +8,27 @@ function Register() {
   const password = useRef();
   const passwordConfirmation = useRef();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // パスワードと確認用パスワードが一致しているか
+    if (password.current.value !== passwordConfirmation.current.value) {
+      passwordConfirmation.current.setCustomValidity(
+        "パスワードが異なります。"
+      );
+    } else {
+      try {
+        const user = {
+          username: username.current.value,
+          email: email.current.value,
+          password: password.current.value,
+        };
+        // Register APIを叩く
+        await axios.post("/auth/register", user);
+      } catch (err) {
+        console.log(err);
+      }
+    }
   };
 
   return (
@@ -26,12 +46,14 @@ function Register() {
               className="loginInput"
               placeholder="ユーザー名"
               required
+              ref={username}
             />
             <input
               type="email"
               className="loginInput"
               placeholder="Eメール"
               required
+              ref={email}
             />
             <input
               type="password"
@@ -39,6 +61,7 @@ function Register() {
               placeholder="パスワード"
               required
               minLength="6"
+              ref={password}
             />
             <input
               type="password"
@@ -46,6 +69,7 @@ function Register() {
               placeholder="確認用パスワード"
               required
               minLength="6"
+              ref={passwordConfirmation}
             />
             <button className="loginButton" type="submit">
               サインアップ
