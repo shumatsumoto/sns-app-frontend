e02392794f5a8bc8d9beda533c9d57930cdb5841
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-08-16 09:41:24 +0900

    画像アップロード実装

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -18,6 +18,20 @@ function Share() {
       desc: desc.current.value,
     };
 
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      newPost.img = fileName;
+      try {
+        // 画像APIを叩く
+        await axios.post("/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
     try {
       await axios.post("/posts", newPost);
       window.location.reload();
